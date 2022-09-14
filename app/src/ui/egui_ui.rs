4eecb085c9565b4c10933184860ede703b9e9df2
Crax <gsolimeno97@gmail.com>
2022-09-15 01:22:37 +0200

    Improve Brush + Layers ui

--- app/src/ui/egui_ui.rs
+++ app/src/ui/egui_ui.rs
@@ -1,4 +1,5 @@
-use egui::{Color32, FontDefinitions, InnerResponse, Label, Pos2, RichText, Sense};
+use bytemuck::Zeroable;
+use egui::{Align2, Color32, FontDefinitions, InnerResponse, Label, Pos2, RichText, Sense, Vec2};
 use egui_wgpu_backend::{RenderPass, ScreenDescriptor};
 use egui_winit_platform::PlatformDescriptor;
 use framework::Framework;
@@ -51,79 +52,77 @@ impl EguiUI {
 }
 
 impl EguiUI {
-    fn brush_settings(&mut self, app_ctx: &mut UiContext) -> bool {
+    fn brush_settings(&mut self, app_ctx: &mut UiContext, ui: &mut egui::Ui) -> bool {
+        ui.label(egui::RichText::new("Brush").heading());
         let mut event_handled = false;
-        let ctx = self.platform.context();
-        let window_handled = egui::Window::new("Brush settings").show(&ctx, |ui| {
-            let engine_config = app_ctx.toolbox.stamping_engine().settings();
-            let mut new_config = engine_config.clone();
+        let engine_config = app_ctx.toolbox.stamping_engine().settings();
+        let mut new_config = engine_config.clone();
 
-            ui.horizontal(|ui| {
-                ui.label("Brush color");
-                ui.color_edit_button_rgba_premultiplied(&mut new_config.color);
-            });
-
-            ui.horizontal(|ui| {
-                ui.label("Brush smoothness");
-                ui.add(egui::Slider::new(&mut new_config.softness, 0.0..=1.0));
-            });
-
-            ui.horizontal(|ui| {
-                ui.add(egui::Checkbox::new(&mut new_config.is_eraser, "Eraser"));
-            });
-
-            if new_config != engine_config {
-                app_ctx.toolbox.update_stamping_engine_data(new_config);
-            }
-
-            ui.separator();
-            ui.label("Brush tool settings");
-            ui.horizontal(|ui| {
-                let max = app_ctx.toolbox.brush_tool.max_size;
-                ui.label("Min brush size");
-                ui.add(
-                    egui::DragValue::new(&mut app_ctx.toolbox.brush_tool.min_size)
-                        .clamp_range(1.0..=max),
-                );
-            });
-            ui.horizontal(|ui| {
-                let min = app_ctx.toolbox.brush_tool.min_size;
-                ui.label("Max brush size");
-                ui.add(
-                    egui::DragValue::new(&mut app_ctx.toolbox.brush_tool.max_size)
-                        .clamp_range(min..=1000.0),
-                );
-            });
-            ui.horizontal(|ui| {
-                ui.label("Step");
-                ui.add(
-                    egui::DragValue::new(&mut app_ctx.toolbox.brush_tool.step)
-                        .clamp_range(1.0..=1000.0),
-                );
-            });
-
-            if ui.button("save test").clicked() {
-                let bytes = app_ctx.image_editor.get_full_image_bytes();
-                if let Some(buffer) =
-                    ImageBuffer::<Rgba<u8>, _>::from_raw(bytes.width, bytes.height, bytes.bytes)
-                {
-                    buffer
-                        .save("image.png")
-                        .unwrap_or_else(|_| println!("Failed to write img"));
-                }
-            }
+        ui.horizontal(|ui| {
+            ui.label("Brush color");
+            ui.color_edit_button_rgba_premultiplied(&mut new_config.color);
         });
-        if let Some(InnerResponse { response, .. }) = window_handled {
-            let mouse_pos = app_ctx.input_state.mouse_position();
-            event_handled = response.rect.contains(Pos2::new(mouse_pos.x, mouse_pos.y));
+
+        ui.horizontal(|ui| {
+            ui.label("Brush smoothness");
+            ui.add(egui::Slider::new(&mut new_config.softness, 0.0..=1.0));
+        });
+
+        ui.horizontal(|ui| {
+            ui.add(egui::Checkbox::new(&mut new_config.is_eraser, "Eraser"));
+        });
+
+        if new_config != engine_config {
+            app_ctx.toolbox.update_stamping_engine_data(new_config);
         }
+
+        ui.separator();
+        ui.label("Brush tool settings");
+        ui.horizontal(|ui| {
+            let max = app_ctx.toolbox.brush_tool.max_size;
+            ui.label("Min brush size");
+            ui.add(
+                egui::DragValue::new(&mut app_ctx.toolbox.brush_tool.min_size)
+                    .clamp_range(1.0..=max),
+            );
+        });
+        ui.horizontal(|ui| {
+            let min = app_ctx.toolbox.brush_tool.min_size;
+            ui.label("Max brush size");
+            ui.add(
+                egui::DragValue::new(&mut app_ctx.toolbox.brush_tool.max_size)
+                    .clamp_range(min..=1000.0),
+            );
+        });
+        ui.horizontal(|ui| {
+            ui.label("Step");
+            ui.add(
+                egui::DragValue::new(&mut app_ctx.toolbox.brush_tool.step)
+                    .clamp_range(1.0..=1000.0),
+            );
+        });
+
+        if ui.button("save test").clicked() {
+            let bytes = app_ctx.image_editor.get_full_image_bytes();
+            if let Some(buffer) =
+                ImageBuffer::<Rgba<u8>, _>::from_raw(bytes.width, bytes.height, bytes.bytes)
+            {
+                buffer
+                    .save("image.png")
+                    .unwrap_or_else(|_| println!("Failed to write img"));
+            }
+        }
+
         event_handled
     }
 
-    fn layer_settings(&mut self, app_ctx: &mut UiContext) -> (bool, LayerAction) {
-        let mut hover = false;
-        let ctx = self.platform.context();
-
+    fn layer_settings(
+        &mut self,
+        app_ctx: &mut UiContext,
+        ui: &mut egui::Ui,
+    ) -> (bool, LayerAction) {
+        ui.separator();
+        ui.label(egui::RichText::new("Layers").heading());
         let document = app_ctx.image_editor.document();
         use image_editor::layers::LayerTree::*;
 
@@ -139,69 +138,60 @@ impl EguiUI {
             }
         };
 
-        let window_handled = egui::Window::new("Layers").show(&ctx, |ui| {
-            if ui
-                .add(egui::Button::new("New layer").sense(sense))
-                .clicked()
-            {
-                action = LayerAction::NewLayerRequest;
-            }
+        if ui
+            .add(egui::Button::new("New layer").sense(sense))
+            .clicked()
+        {
+            action = LayerAction::NewLayerRequest;
+        }
 
-            let mut lay_layer_ui = |idx: &LayerIndex| {
-                let original_settings = document.get_layer(idx).settings();
-                let color = if *idx == document.current_layer_index {
-                    Color32::LIGHT_BLUE
-                } else {
-                    Color32::WHITE
-                };
-                let mut settings = original_settings.clone();
-
-                ui.horizontal(|ui| {
-                    ui.visuals_mut().extreme_bg_color = color;
-                    if ui
-                        .add(Label::new(RichText::from(&settings.name).color(color)).sense(sense))
-                        .clicked()
-                    {
-                        action = LayerAction::SelectLayer(idx.clone());
-                    }
-
-                    ui.add(egui::Checkbox::new(&mut settings.is_enabled, ""));
-
-                    if ui
-                        .add(egui::Button::new("Delete layer").sense(sense))
-                        .clicked()
-                    {
-                        action = LayerAction::DeleteLayer(idx.clone());
-                    }
-                });
-
-                ui.add(egui::Slider::new(&mut settings.opacity, 0.0..=1.0).text("Opacity"));
-
-                if settings != original_settings {
-                    action = LayerAction::SetLayerSettings(idx.clone(), settings);
-                }
+        let mut lay_layer_ui = |idx: &LayerIndex| {
+            let original_settings = document.get_layer(idx).settings();
+            let color = if *idx == document.current_layer_index {
+                Color32::LIGHT_BLUE
+            } else {
+                Color32::WHITE
             };
-            for layer in document.tree_root.0.iter() {
-                match layer {
-                    SingleLayer(idx) => {
+            let mut settings = original_settings.clone();
+
+            ui.horizontal(|ui| {
+                if ui
+                    .add(Label::new(RichText::from(&settings.name).color(color)).sense(sense))
+                    .clicked()
+                {
+                    action = LayerAction::SelectLayer(idx.clone());
+                }
+
+                ui.add(egui::Checkbox::new(&mut settings.is_enabled, ""));
+
+                if ui
+                    .add(egui::Button::new("Delete layer").sense(sense))
+                    .clicked()
+                {
+                    action = LayerAction::DeleteLayer(idx.clone());
+                }
+            });
+
+            ui.add(egui::Slider::new(&mut settings.opacity, 0.0..=1.0).text("Opacity"));
+
+            if settings != original_settings {
+                action = LayerAction::SetLayerSettings(idx.clone(), settings);
+            }
+        };
+        for layer in document.tree_root.0.iter() {
+            match layer {
+                SingleLayer(idx) => {
+                    lay_layer_ui(idx);
+                }
+                Group(indices) => {
+                    for idx in indices.iter() {
                         lay_layer_ui(idx);
                     }
-                    Group(indices) => {
-                        for idx in indices.iter() {
-                            lay_layer_ui(idx);
-                        }
-                    }
                 }
             }
-        });
+        }
 
-        hover |= if let Some(InnerResponse { response, .. }) = window_handled {
-            let mouse_pos = app_ctx.input_state.mouse_position();
-            response.rect.contains(Pos2::new(mouse_pos.x, mouse_pos.y))
-        } else {
-            false
-        };
-        (hover, action)
+        (false, action)
     }
 
     fn new_layer_dialog(&mut self, app_ctx: &mut UiContext) -> LayerAction {
@@ -209,8 +199,6 @@ impl EguiUI {
         let mut action = LayerAction::None;
         egui::Window::new("Create new layer")
             .anchor(egui::Align2::CENTER_CENTER, egui::vec2(0.0, 0.0))
-            .resizable(false)
-            .collapsible(false)
             .show(&ctx, |ui| {
                 let layer_settings = self.new_layer_in_creation.as_mut().unwrap();
 
@@ -218,9 +206,13 @@ impl EguiUI {
                 ui.color_edit_button_rgba_unmultiplied(&mut layer_settings.initial_color);
                 ui.label("Layer name?");
                 ui.text_edit_singleline(&mut layer_settings.name);
-
+                if layer_settings.name.is_empty() {
+                    egui::containers::show_tooltip(&ctx, egui::Id::new("invalid-layer"), |ui| {
+                        ui.label("Cannot create a layer with an empty name!");
+                    });
+                }
                 if ui.button("Create").clicked() && !layer_settings.name.is_empty() {
-                    action = LayerAction::CreateNewLayer;
+                    action = LayerAction::CreateNewLayer
                 } else if ui.button("Cancel").clicked() {
                     action = LayerAction::CancelNewLayerRequest
                 } else {
@@ -261,23 +253,29 @@ impl Ui for EguiUI {
                 }
             }
         } else {
-            let brush = self.brush_settings(&mut app_ctx);
-            let (hover_layer, layer_action) = self.layer_settings(&mut app_ctx);
+            let ctx = self.platform.context();
+            let mut res = false;
+            egui::Window::new("")
+                .anchor(Align2::LEFT_CENTER, Vec2::zeroed())
+                .show(&ctx, |ui| {
+                    res |= self.brush_settings(&mut app_ctx, ui);
+                    let (hover_layer, layer_action) = self.layer_settings(&mut app_ctx, ui);
+                    res |= hover_layer;
 
-            match layer_action {
-                LayerAction::NewLayerRequest => {
-                    self.new_layer_in_creation = Some(LayerConstructionInfo::default());
-                    return true;
-                }
-                LayerAction::DeleteLayer(idx) => app_ctx.image_editor.delete_layer(idx),
-                LayerAction::SelectLayer(idx) => app_ctx.image_editor.select_new_layer(idx),
-                LayerAction::SetLayerSettings(idx, settings) => {
-                    let document = app_ctx.image_editor.mutate_document();
-                    document.get_layer_mut(&idx).set_settings(settings);
-                }
-                _ => {}
-            }
-            brush || hover_layer
+                    match layer_action {
+                        LayerAction::NewLayerRequest => {
+                            self.new_layer_in_creation = Some(LayerConstructionInfo::default());
+                        }
+                        LayerAction::DeleteLayer(idx) => app_ctx.image_editor.delete_layer(idx),
+                        LayerAction::SelectLayer(idx) => app_ctx.image_editor.select_new_layer(idx),
+                        LayerAction::SetLayerSettings(idx, settings) => {
+                            let document = app_ctx.image_editor.mutate_document();
+                            document.get_layer_mut(&idx).set_settings(settings);
+                        }
+                        _ => {}
+                    };
+                });
+            res
         }
     }
     fn present(
