81640694c7a37f90286acdce65263c0061d7f89e
Crax97 <gsolimeno97@gmail.com>
2022-10-11 02:37:57 +0200

    App: remove lazy_static

--- app/src/main.rs
+++ app/src/main.rs
@@ -12,10 +12,9 @@ use lazy_static::lazy_static;
 use tools::*;
 
 use winit::dpi::PhysicalSize;
-use winit::platform::run_return::EventLoopExtRunReturn;
 
 async fn run_app() -> anyhow::Result<()> {
-    let mut event_loop = winit::event_loop::EventLoop::new();
+    let event_loop = winit::event_loop::EventLoop::new();
     let window = winit::window::WindowBuilder::new()
         .with_title("Image editor")
         .with_min_inner_size(PhysicalSize {
@@ -23,7 +22,8 @@ async fn run_app() -> anyhow::Result<()> {
             height: 600,
         })
         .build(&event_loop)?;
-    let framework = {
+
+    let framework = Box::leak(Box::new({
         let framework = Framework::new(&wgpu::DeviceDescriptor {
             label: Some("Image Editor framework"),
             features: wgpu::Features::empty(),
@@ -39,13 +39,12 @@ async fn run_app() -> anyhow::Result<()> {
                 panic!("Error while creating framework: {}", e)
             }
         }
-    };
-    let mut app_state = ImageApplication::new(window, &framework);
+    }));
+    let app_state = Box::leak(Box::new(ImageApplication::new(window, framework)));
 
-    event_loop.run_return(move |event, _, control_flow| {
+    event_loop.run(move |event, _, control_flow| {
         *control_flow = app_state.on_event(&event);
     });
-    Ok(())
 }
 
 fn main() {
